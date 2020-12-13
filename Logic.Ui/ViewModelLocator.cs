360e62ff8f22e7293258d37d123089c0cb0eab6e
Jan-Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2020-12-13 10:39:33 +0100

    Adding Statistics Window an VM -Philipp

--- Logic.Ui/ViewModelLocator.cs
+++ Logic.Ui/ViewModelLocator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui
     public class ViewModelLocator
     {
 
-
+        public StatisticViewModel StatisticViewModel { get; }
         public ViewModelLocator()
         {
 
