6243cd54e3dde28aa7151e3379bdede113bc28b7
Hauke Marquard <hauke.marquard@gmail.com>
2020-12-16 12:25:31 +0100

    ViewModels angelegt

--- Logic.Ui/ViewModelLocator.cs
+++ Logic.Ui/ViewModelLocator.cs
@@ -11,6 +11,8 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui
     {
 
         public StatisticViewModel StatisticViewModel { get; }
+        public ImportViewModel ImportViewModel { get; }
+        public ExportViewModel ExportViewMode { get; }
         public ViewModelLocator()
         {
 
