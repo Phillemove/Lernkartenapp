15ee34e2338246f7e28c29f134cc5c880879a606
Christian Oehlers <averodas@gmail.com>
2021-01-05 14:17:10 +0100

    Changes to Model, created ModelViewModel Wrapper Stub

--- Business.Model/BusinessObjects/Model.cs
+++ Business.Model/BusinessObjects/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,24 @@ namespace De.HsFlensburg.ClientApp101.Business.Model.BusinessObjects
     {
         public CategoryCollection CategoryCollection { get; set; }
         public BoxCollection BoxCollection { get; set; }
+
+        private const String CategoryFile = "../../Data/Categorys.xml"; // Pfad zur Kategoriedatei
+
+        public Model()
+        {
+            // Load Category Collection
+            LoadCategorys();
+        }
+
+        private void LoadCategorys()
+        {
+            // TODO
+            /**
+            if (File.Exists(CategoryFile))
+            {
+                Console.WriteLine("its there");
+            }
+    **/
+        }
     }
 }
---
+++ Logic.Ui/Wrapper/ModelViewModel.cs
@@ -0,0 +1,19 @@
+﻿using De.HsFlensburg.ClientApp101.Business.Model.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp101.Logic.Ui.Wrapper
+{
+    public class ModelViewModel
+    {
+        
+        public ModelViewModel()
+        {
+            // Model erzeugen - damit Konstruktor für CategoryCollection undso aufrufen - auch Box - Enum
+        }
+
+    }
+}
