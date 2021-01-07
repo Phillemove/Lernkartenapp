8a9e2f4f44ccdcbf8b20fc90d876b5eab24a7a50
Jan-Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2021-01-07 09:52:08 +0100

    PT Set DataGrid ReadOnly and create DummyData

--- Logic.Ui/ViewModelLocator.cs
+++ Logic.Ui/ViewModelLocator.cs
@@ -1,4 +1,5 @@
-﻿using De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels;
+﻿using De.HsFlensburg.ClientApp101.Business.Model.BusinessObjects;
+using De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels;
 using De.HsFlensburg.ClientApp101.Logic.Ui.Wrapper;
 using System;
 using System.Collections.Generic;
@@ -32,6 +33,17 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui
             CardLearningViewModel = new CardLearningViewModel();
             CardWindowViewModel = new CardWindowViewModel();
             CategoryViewModel = new CategoryViewModel();
+
+            // Statistic DummyDaten
+            Statistic statistic = new Statistic();
+            statistic.Timestamp = DateTime.Now;
+            statistic.SuccessfullAnswer = true;
+            statisticCollectionVM.Add(new StatisticViewModel(statistic));
+
+            Statistic statistic2 = new Statistic();
+            statistic2.Timestamp = DateTime.UtcNow;
+            statistic2.SuccessfullAnswer = false;
+            statisticCollectionVM.Add(new StatisticViewModel(statistic2));
 
         }
     }
