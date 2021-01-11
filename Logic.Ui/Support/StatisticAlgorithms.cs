6b907775dfd33292c3ea39945d02ef3c9819c9d0
Jan-Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2021-01-11 11:01:36 +0100

    PT Adding more Statistics

--- Logic.Ui/Support/StatisticAlgorithms.cs
+++ Logic.Ui/Support/StatisticAlgorithms.cs
@@ -10,7 +10,7 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.Support
 {
     public class StatisticAlgorithms
     {
-        // How often ist this card learned successfull
+        // How often is this card learned successfull
         public static int RightAnswer(StatisticCollectionViewModel collection)
         {
             int counter = 0;
@@ -19,13 +19,12 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.Support
                 if (statistic.SuccessfulAnswer == true)
                 {   
                     counter++;
-                    Console.WriteLine(counter);
                 }
             }
             return counter;
         }
 
-        // How often ist this card learned
+        // How often is this card learned
         public static int LearnedCard(StatisticCollectionViewModel collection)
         {
             int counter = 0;
@@ -36,7 +35,7 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.Support
             return counter;
         }
 
-        // On whitch Date ist a Card learned correct the last time
+        // On which Date is a Card learned correct the last time
         public static DateTime LastSucessfullAnswer(StatisticCollectionViewModel collection)
         {
             DateTime time = new DateTime();
@@ -77,6 +76,98 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.Support
                     counter++;
                     break;
                 } 
+            }
+            return counter;
+        }
+
+        // When is the Card learned last
+        public static DateTime LearnedLast(StatisticCollectionViewModel collection)
+        {
+            DateTime time = new DateTime();
+            foreach(StatisticViewModel statistic in collection)
+            {
+                if (DateTime.Compare(time, statistic.Timestamp) < 0)
+                {
+                    time = statistic.Timestamp;
+                }
+            }
+            return time;
+        }
+
+        // Last Answer
+        public static bool LastAnswer(StatisticCollectionViewModel collection)
+        {
+            bool lastanswer = false;
+            DateTime time = new DateTime();
+            foreach (StatisticViewModel statistic in collection)
+            {
+                if (DateTime.Compare(time, statistic.Timestamp) < 0)
+                {
+                    time = statistic.Timestamp;
+                    lastanswer = statistic.SuccessfulAnswer;
+                }
+            }
+            return lastanswer;
+        }
+
+        // How often ist this card learned unsuccessfull
+        public static int WrongAnswer(StatisticCollectionViewModel collection)
+        {
+            int counter = 0;
+            foreach (StatisticViewModel statistic in collection)
+            {
+                if (statistic.SuccessfulAnswer == false)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        // On which Date is a Card learned wrong the last time
+        public static DateTime LastWrongAnswer(StatisticCollectionViewModel collection)
+        {
+            DateTime time = new DateTime();
+            foreach (StatisticViewModel statistic in collection)
+            {
+                if (statistic.SuccessfulAnswer == false)
+                {
+                    if (DateTime.Compare(time, statistic.Timestamp) < 0)
+                    {
+                        time = statistic.Timestamp;
+                    }
+
+                }
+            }
+            return time;
+        }
+
+        // How often in a row is a Card learned Wrong
+        public static int LearningWrong(StatisticCollectionViewModel collection)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+
+                if (i + 1 < collection.Count && collection[i].SuccessfulAnswer == true && collection[i + 1].SuccessfulAnswer == false)
+                {
+
+                }
+                else if (i + 1 < collection.Count && collection[i].SuccessfulAnswer == false && collection[i + 1].SuccessfulAnswer == false)
+                {
+                    counter++;
+                }
+                else if (i + 1 < collection.Count && collection[i].SuccessfulAnswer == false && collection[i + 1].SuccessfulAnswer == true)
+                {
+                    counter++;
+                    break;
+                }
+                else if (i + 1 >= collection.Count && collection[i].SuccessfulAnswer == false)
+                {
+                    counter++;
+                    break;
+                }
             }
             return counter;
         }
--- Logic.Ui/ViewModels/StatisticWindowViewModel.cs
+++ Logic.Ui/ViewModels/StatisticWindowViewModel.cs
@@ -10,44 +10,34 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels
 {
     public class StatisticWindowViewModel
     {
-        private bool _showDataGrid;
-        public RelayCommand ShowHide { get; }
         public StatisticCollectionViewModel StatisticCollectionVM { get; set; }
         public int LearnedCard { get; set; }
         public int RightAnswer { get; set; }
         public DateTime LastSucessfullAnswer { get; set; }
         public int LearningSucess { get; set; }
-        public bool ShowDataGrid
-        {
-            get { return _showDataGrid; }
-        }
+        public DateTime LastLearned { get; set; }
+        public bool LastAnswer { get; set; }
+        public int WrongAnswer { get; set; }
+        public DateTime LastWrongAnswer { get; set; }
+        public int LearningWrong { get; set; }
+
         public StatisticWindowViewModel(StatisticCollectionViewModel statisticCollectionVM)
         {
-            
-            ShowHide = new RelayCommand(() => ShowHideMethod());
             StatisticCollectionVM = statisticCollectionVM;
-            _showDataGrid = true;
         }
 
-        private void ShowHideMethod()
-        {
-            if (_showDataGrid == true)
-            {
-                _showDataGrid = false;
-                
-            } else
-            {
-                _showDataGrid = true;
-                
-            }
-        }
-
+        // Writing statistics for the Window
         public void MakeStatistic()
         {
             LearnedCard = StatisticAlgorithms.LearnedCard(StatisticCollectionVM);
             RightAnswer = StatisticAlgorithms.RightAnswer(StatisticCollectionVM);
             LastSucessfullAnswer = StatisticAlgorithms.LastSucessfullAnswer(StatisticCollectionVM);
             LearningSucess = StatisticAlgorithms.LearningSucess(StatisticCollectionVM);
+            LastLearned = StatisticAlgorithms.LearnedLast(StatisticCollectionVM);
+            LastAnswer = StatisticAlgorithms.LastAnswer(StatisticCollectionVM);
+            WrongAnswer = StatisticAlgorithms.WrongAnswer(StatisticCollectionVM);
+            LastWrongAnswer = StatisticAlgorithms.LastWrongAnswer(StatisticCollectionVM);
+            LearningWrong = StatisticAlgorithms.LearningWrong(StatisticCollectionVM);
         }
 
     }
