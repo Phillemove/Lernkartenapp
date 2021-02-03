3e5e1262d307780655296a449b5768c88a2aa79f
Maha Alshamali <maha.alshamali2@stud.hs-flensburg.de>
2021-02-03 19:00:44 +0100

    Maha_Kartebearbeiten_Karte hinzufügen

--- Ui.Desktop/CardAdd.xaml.cs
+++ Ui.Desktop/CardAdd.xaml.cs
@@ -23,5 +23,10 @@ namespace De.HsFlensburg.ClientApp101.Ui.Desktop
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
