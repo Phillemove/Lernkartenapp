490e84671b8d4bdfa22dcc5c6fa092e54b1c7001
Christian Oehlers <averodas@gmail.com>
2021-02-06 15:26:33 +0100

    ImportFF and CategoryManage Changes

--- Logic.Ui/ViewModels/ImportForeignFormatViewModel.cs
+++ Logic.Ui/ViewModels/ImportForeignFormatViewModel.cs
@@ -14,10 +14,15 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels
     {
         public CategoryCollectionViewModel MyCategoryCollection { get; set; }
         public BoxViewModel ImportBox { get; }
+        public CategoryViewModel SelectedCategory { get; set; }
+        public BoxViewModel TempBox { get; set; }
 
         public RelayCommand CloseWindow { get; }
         public RelayCommand SelectFile { get; }
+        public RelayCommand FileImport { get; }
+        public RelayCommand CardImport { get; }
         public String ErrorMsg { get; set; }
+        public String ImportFile { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
 
 
@@ -25,20 +30,33 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels
         {
             CloseWindow = new RelayCommand(param => Close(param));
             SelectFile = new RelayCommand(() => FileChoose());
+            FileImport = new RelayCommand(() => Import());
+            CardImport = new RelayCommand(() => ImportCards());
             this.MyCategoryCollection = ccvm;
             this.ImportBox = importbox;
+            this.TempBox = new BoxViewModel();
+            CardViewModel card = new CardViewModel();
+            card.StatisticCollection = new StatisticCollectionViewModel();
+            card.Question = "Title";
+            card.Answer = "Answer";
+            TempBox.Add(card);
+
+
             ErrorMsg = "";
+            ImportFile = "";
         }
 
         // Für Änderungen der Fehlermeldung
         protected void OnPropertyChanged(string propertyName)
         {
-            PropertyChanged?.Invoke(this,
-                new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
 
+        private void ImportCards()
+        {
 
+        }
         private void Close(object param)
         {
             // Save Box
@@ -56,19 +74,118 @@ namespace De.HsFlensburg.ClientApp101.Logic.Ui.ViewModels
             ErrorMsg = msg;
             OnPropertyChanged("ErrorMsg");
         }
+        private void ClearFile()
+        {
+            ImportFile = "";
+            OnPropertyChanged("ImportFile");
+        }
+        private void ShowFile(String msg)
+        {
+            ImportFile = msg;
+            OnPropertyChanged("ImportFile");
+        }
+
+        private String StripHTML(String str)
+        {
+            str = str.Replace("<br>","\n");
+
+            return str;
+        }
+
+        private void ImportCoboCards()
+        {
+            var reader = System.Xml.XmlReader.Create(ImportFile);
+            if (reader.ReadToFollowing("CoboCards"))
+            {
+                // Ist Cobo Cards Export
+                while (reader.ReadToFollowing("card"))
+                {
+                    if (reader.HasAttributes)
+                    {
+                        String title = StripHTML(reader.GetAttribute("title"));
+                        String content = StripHTML(reader.GetAttribute("content"));
+                        if (title != null && content != null)
+                        {
+
+                            CardViewModel card = new CardViewModel();
+                            card.StatisticCollection = new StatisticCollectionViewModel();
+                            card.Question = title;
+                            card.Answer = content;
+                            TempBox.Add(card);
+                            OnPropertyChanged("TempBox");
+                            Console.WriteLine(card.Answer);
+                            //show cards?
+                            // Save Box
+                            // Exclude Picture only answers
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ShowError("XML File ist kein valider CoboCards Export");
+            }
+        }
+
 
         private void FileChoose()
         {
             ClearError();
             OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "CoboCards Export|*.xml| Ansi txt-Export|*.txt";
             if (dialog.ShowDialog() == DialogResult.OK)
             {
-
+                
+                if (dialog.FileName.EndsWith(".txt"))
+                {
+                    ShowFile(dialog.FileName);
+                    ShowError("txt nicht implementiert");
+                }
+                else if (dialog.FileName.EndsWith(".xml"))
+                {
+                    ShowFile(dialog.FileName);
+                }
+                else
+                {
+                    ShowError("Nur .txt und .xml Dateien erlaubt");
+                }
             }
             else
             {
                 ShowError("Keine Datei ausgewählt");
             }
         }
+
+        private void Import()
+        {
+            if (ImportFile != "")
+            {
+                if (SelectedCategory != null)
+                {
+                    if (ImportFile.EndsWith(".txt"))
+                    {
+
+                    }
+                    else if (ImportFile.EndsWith(".xml"))
+                    {
+                        ImportCoboCards();
+                    }
+                    else
+                    {
+                        ShowError("Kein valides Dateiformat");
+                    }
+                }
+                else
+                {
+                    ShowError("Keine Kategorie ausgewählt");
+                }
+            }
+            else
+            {
+                ShowError("Keine Datei ausgewählt");
+            }
+        }
+
+
     }
 }
